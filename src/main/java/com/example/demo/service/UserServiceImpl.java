437e49317a87357a64c4a9de55c8dd87b54b9958
2071032@tcs.com <2071032@tcs.com>
2021-12-08 17:45:47 +0000

    Day 2

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,16 +1,21 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
 
 @Service
 public class UserServiceImpl implements IUserService{
 
+	@Autowired
+	UserRepository repository;
+	
 	@Override
 	public String save(User user) {
-		// TODO Auto-generated method stub
 		System.out.println(user.getAge());
+		repository.save(user);
 		return null;
 	}
 
